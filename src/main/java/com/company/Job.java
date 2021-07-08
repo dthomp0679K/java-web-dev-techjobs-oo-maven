d7e97e15c868aa27b4a2d7ea108234db84c2bc91
dthomp0679K <derek_thompson@cable.comcast.com>
2021-07-08 09:59:44 -0500

    second add

--- src/main/java/com/company/Job.java
+++ src/main/java/com/company/Job.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Comparator;
+import java.util.Objects;
 public class Job {
     private static int nextId = 1;
 
@@ -15,4 +17,72 @@ public class Job {
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields match.
 
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID and id.
+
+    public Job() {
+        id = nextId;
+            nextId++;
+    }
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Job)) return false;
+        Job job = (Job) o;
+        return getId() == job.getId();
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(PositionType positionType) {
+        this.positionType = positionType;
+    }
+
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setCoreCompetency(CoreCompetency value) {
+        this.coreCompetency = value;
+    }
 }
--- src/test/java/com/company/incomplete/JobTests.java
+++ src/test/java/com/company/incomplete/JobTests.java
@@ -1,22 +1,69 @@
 package com.company.incomplete;
-
+import static org.junit.jupiter.api.Assertions.*;
+import com.company.Job;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import com.company.CoreCompetency;
+import com.company.Employer;
+import com.company.PositionType;
+import com.company.Main;
+import com.company.Location;
+
+
 
 public class JobTests {
+
+    @Before
+    public void createJobObjects(){
+        Job testJob1 = new Job();
+        Job testJob2 = new Job();
+    }
     // you can rename the tests, or delete them and rewrite them from scratch, but I went ahead and provided you the names of tests I created -- I followed the writeup pretty honestly
+    Job testJob1 = new Job();
+    Job testJob2 = new Job();
+
     @Test
     public void testSettingJobId() {
+       assertFalse(testJob1.equals(testJob2));
+       assertTrue(testJob2.getId() - 1 == testJob1.getId());
+
+        //id values are not the same
+        //id values differ by one
 
     }
 
+
+
     @Test
     public void testJobConstructorSetsAllFields() {
+        // declared new job object
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
+      //Use assert statements to test that the constructor correctly assigns the class and value of each field.
+assertTrue(testJob instanceof Job);
+//The instanceof keyword can be used to check the class of an object. The result of the comparison is a boolean.
+assertTrue(testJob.getName() == "Product tester");
+
+assertTrue(testJob.getEmployer() instanceof Employer);
+assertTrue(testJob.getEmployer().getValue() ==  "ACME");
+
+assertTrue(testJob.getLocation() instanceof Location);
+assertTrue(testJob.getLocation().getValue() == "Desert");
+
+assertTrue(testJob.getPositionType() instanceof PositionType);
+assertTrue(testJob.getPositionType().getValue() == "Quality control");
+
+assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+assertTrue(testJob.getCoreCompetency().getValue() == "Persistence");
     }
 
     @Test
     public void testJobsForEquality() {
-
+        Job Job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));;
+        Job Job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));;
+// test chekcing if these two object are false.
+    assertFalse(Job1.equals(Job2));
     }
 
     // I found the instructions around the three toString() tests a little confusing, so I created three tests following the three bullet points:
